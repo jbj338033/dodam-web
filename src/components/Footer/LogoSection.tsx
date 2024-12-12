b7346b5d5e87e242407ca7a0286d0e4cb1542df4
전민오 <jbj338033@gmail.com>
2024-12-12 22:14:43 +0900

    feat: logo section

---
+++ src/components/Footer/LogoSection.tsx
@@ -0,0 +1,13 @@
+const LogoSection = () => {
+  return (
+    <div className="flex items-center gap-2">
+      <img src="/b1nd.svg" alt="B1ND Logo" className="h-6" />
+      <p className="text-sm text-slate-400">
+        도담도담은 현재 대구소프트웨어마이스터고등학교에서 사용 중인 스마트 학생
+        생활 관리 시스템입니다.
+      </p>
+    </div>
+  );
+};
+
+export default LogoSection;
