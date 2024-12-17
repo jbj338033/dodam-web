4f43b2721052f0cec15491afa8e4929ac9d85b25
전민오 <jbj338033@gmail.com>
2024-12-17 09:46:46 +0900

    feat: optimize layout;

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,26 @@
+import { memo } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 
-export default function Layout() {
+const MainContent = memo(() => (
+  <main className="flex-1">
+    <Outlet />
+  </main>
+));
+
+MainContent.displayName = "MainContent";
+
+const Layout = memo(() => {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-
-      <main className="flex-1">
-        <Outlet />
-      </main>
-
+      <MainContent />
       <Footer />
     </div>
   );
-}
+});
+
+Layout.displayName = "Layout";
+
+export default Layout;
